d8bf230b4cda841c730921902536e592a79c0e86
androo.gardner@gmail.com <androo.gardner@gmail.com@f3b4058f-8ea4-8f77-95d1-b9d5bc24aafc>
2011-09-10 15:46:01 +0000

    Version bump!
    git-svn-id: http://sproxel.googlecode.com/svn/trunk@140 f3b4058f-8ea4-8f77-95d1-b9d5bc24aafc

--- MainWindow.h
+++ MainWindow.h
@@ -13,7 +13,7 @@
 #include "PaletteWidget.h"
 #include "GLModelWidget.h"
 
-#define SPROXEL_VERSION "0.4"
+#define SPROXEL_VERSION "0.5"
 #define BASE_WINDOW_TITLE (tr("Sproxel " SPROXEL_VERSION))
 
 class MainWindow : public QMainWindow
